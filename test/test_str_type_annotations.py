3bdfd5006e529ce9edd3f25c7e35b513e4061327
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2024-09-04 10:00:24 +1200

    Test using string type annotations

---
+++ test/test_str_type_annotations.py
@@ -0,0 +1,21 @@
+from typing_extensions import Annotated
+
+import dataclasses_struct as dcs
+
+
+def test_str_type_annotations() -> None:
+    @dcs.dataclass(size='std')
+    class _:
+        a: 'dcs.Char'
+        b: 'dcs.I8'
+        c: 'dcs.U8'
+        d: 'dcs.Bool'
+        e: 'dcs.I16'
+        f: 'dcs.U16'
+        g: 'dcs.I32'
+        h: 'dcs.U32'
+        i: 'dcs.I64'
+        j: 'dcs.U64'
+        k: 'dcs.F32'
+        l: 'dcs.F64'  # noqa: E741
+        m: 'Annotated[bytes, dcs.BytesField(10)]'
