7f02b8c1e439bf76cf860c4772b430abc15c9124
Harry Mander <harry.mander@pg.canterbury.ac.nz>
2024-11-07 10:08:40 +1300

    Restructure C struct test a little

--- test/test_cstruct.py
+++ test/test_cstruct.py
@@ -1,8 +1,6 @@
-import shutil
 import subprocess
 import sys
 from pathlib import Path
-from typing import Sequence
 
 import pytest
 
@@ -14,71 +12,53 @@ def run(*args: str) -> None:
 
 
 if sys.platform.startswith('win'):
-    CC = 'cl.exe'
-
-    def build_cc_args(
-        dir: Path, exe_path: Path, native: bool
-    ) -> Sequence[str]:
-        return (
-            CC,
+    def _compile_cstruct(outdir: Path, native: bool) -> Path:
+        exe_path = outdir / 'struct-tester.exe'
+        run(
+            'cl.exe',
             'test\\struct.c',
-            f'/Fo:{dir}',
+            f'/Fo:{outdir}',
             '/WX',
             f'/{"D" if native else "U"}TEST_NATIVE_INTS',
             '/link', f'/out:{exe_path}',
         )
-else:
-    CC = 'cc'
+        return exe_path
 
-    def build_cc_args(
-        dir: Path, exe_path: Path, native: bool
-    ) -> Sequence[str]:
-        return (
-            CC,
+else:
+    def _compile_cstruct(outdir: Path, native: bool) -> Path:
+        exe_path = outdir / 'struct-tester'
+        run(
+            'cc',
             '-o', str(exe_path),
             'test/struct.c',
             '-Wall', '-Werror',
             f'-{"D" if native else "U"}TEST_NATIVE_INTS',
         )
+        return exe_path
 
 
-def _struct_tester(dir: Path, native: bool) -> Path:
-    assert shutil.which(CC), f"Cannot find C compiler '{CC}'"
-    exe = dir / 'struct-tester.exe'
-    args = build_cc_args(dir, exe, native)
-    run(*args)
-    return exe
-
-
-@pytest.fixture
-def struct_tester(tmp_path: Path) -> Path:
-    return _struct_tester(tmp_path, native=False)
-
-
-@pytest.fixture
-def native_struct_tester(tmp_path: Path) -> Path:
-    return _struct_tester(tmp_path, native=True)
-
-
-@pytest.fixture
-def cstruct(tmp_path: Path, struct_tester: Path) -> Path:
+def _cstruct(tmp_path: Path, native: bool) -> Path:
+    exe = _compile_cstruct(tmp_path, native)
     outpath = tmp_path / 'struct'
-    run(str(struct_tester), str(outpath))
+    run(str(exe), str(outpath))
     return outpath
 
 
 @pytest.fixture
-def native_cstruct(tmp_path: Path, native_struct_tester: Path) -> Path:
-    outpath = tmp_path / 'struct'
-    run(str(native_struct_tester), str(outpath))
-    return outpath
+def native_cstruct(tmp_path: Path) -> Path:
+    return _cstruct(tmp_path, native=True)
 
 
 @pytest.mark.xfail
-def test_unpack_from_cstruct_with_native_size():
+def test_unpack_from_cstruct_with_native_size(native_cstruct):
     assert False, "TODO"
 
 
+@pytest.fixture
+def std_cstruct(tmp_path: Path) -> Path:
+    return _cstruct(tmp_path, native=False)
+
+
 @pytest.mark.xfail
-def test_unpack_from_cstruct_with_std_size():
+def test_unpack_from_cstruct_with_std_size(std_cstruct):
     assert False, "TODO"
